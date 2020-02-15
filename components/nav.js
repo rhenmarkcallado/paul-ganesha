b0111bb51aa0e79040b6e7114c56799f35ca3f61
Rhen Mark Callado <rhenmarkc@gmail.com>
2020-02-15 23:14:53 +0800

    make it better

--- components/nav.js
+++ components/nav.js
@@ -1,56 +1,118 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { memo } from "react";
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
-].map(link => ({
-  ...link,
-  key: `nav-link-${link.href}-${link.label}`,
-}))
+  {
+    label: "Our Story",
+    link: "story"
+  },
+  {
+    label: "Proposal",
+    link: "proposal"
+  },
+  {
+    label: "Details",
+    link: "details"
+  },
+  {
+    label: "Invitations",
+    link: "invitations"
+  },
+  {
+    label: "Photos",
+    link: "photos"
+  }
+];
 
-const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
-    </ul>
+const Nav = ({ top }) => {
+  const isScrolling = Boolean(top < -100);
+  return (
+    <nav className={isScrolling ? "with-background" : ""}>
+      <div className={` ${isScrolling ? "scrolling" : "logo"} `}>
+        <a href="#">
+          Paul & <br /> Ganesha
+        </a>
+      </div>
+      <div className="navbar">
+        <ul>
+          {links.map(item => (
+            <li key={item.label}>
+              <a href={`#${item.link}`}>{item.label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
-  </nav>
-)
+      <style jsx>{`
+        :global(body) {
+          margin: 0;
+          padding: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
+            Helvetica, sans-serif;
+          color: white;
+          background-color: #fff;
+        }
 
-export default Nav
+        ul {
+          list-style: none;
+        }
+
+        nav {
+          display: grid;
+          grid-template-columns: 40% 60%;
+          padding: 20px;
+          position: relative;
+          z-index: 2;
+          position: sticky;
+          top: 0;
+        }
+
+        .with-background {
+          z-index: 10;
+          background-color: rgba(0, 0, 0, 0.75);
+        }
+
+        nav > .logo a {
+          color: #fff;
+          text-decoration: none;
+          font-size: 32px;
+        }
+
+        nav > .logo > a {
+          position: absolute;
+          padding-left: 62px;
+          top: 160px;
+          font-size: 70px;
+          color: #ffffff;
+          text-decoration-style: none;
+          text-rendering: optimizeSpeed;
+          text-shadow: -2px 2px 1px #ffa401;
+        }
+
+        nav > .scrolling > a {
+          color: #ffffff;
+          position: relative;
+          font-size: 18px;
+          text-shadow: none;
+          top: 0;
+          padding-left: 0;
+          text-decoration: none;
+          border: 1px solid #fff;
+        }
+
+        nav > .navbar > ul {
+          display: flex;
+          flex-direction: row;
+          justify-content: flex-end;
+        }
+
+        nav > .navbar > ul > li a {
+          margin-left: 30px;
+          text-decoration: none;
+          color: #fff;
+        }
+      `}</style>
+    </nav>
+  );
+};
+
+export default memo(Nav);
--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,152 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React, { useLayoutEffect, useState } from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = () => {
+  const [scrollTopPx, setScrollTopPx] = useState(0);
+  useLayoutEffect(() => {
+    const handleScroll = e => {
+      const body = e.target.body.getBoundingClientRect();
+      if (body && scrollTopPx !== body.top) {
+        setScrollTopPx(body.top);
+      }
+    };
 
-    <Nav />
+    if (typeof window !== undefined && window) {
+      window.addEventListener("scroll", handleScroll);
+    }
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+  return (
+    <div className="home">
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav top={scrollTopPx} />
+
+      <div className="landing">
+        <div className="landing-container">
+          <div className="info">
+            <span>Save the Date</span>
+            <span className="date">X.II.MMXX</span>
+            <button>see details</button>
+          </div>
+        </div>
       </div>
+
+      <div className="story">
+        <span>Our story</span>
+        <div className="description">
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </span>
+        </div>
+      </div>
+      <div className="details">
+        <span>Details</span>
+      </div>
+
+      <style jsx>{`
+        .landing {
+          background: url("https://images.unsplash.com/photo-1531747056595-07f6cbbe10ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80");
+          background-size: cover;
+          background-repeat: no-repeat;
+          position: relative;
+          height: 100vh;
+          left: 0;
+          right: 0;
+          top: -90px;
+          z-index: 1;
+
+          background-attachment: fixed;
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+        }
+
+        .landing-container {
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          width: 100%;
+          height: inherit;
+          padding: 50px;
+          box-sizing: border-box;
+        }
+
+        .info {
+          height: 300px;
+          width: 300px;
+          color: #000;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+        }
+        .info > span {
+          color: #fff;
+        }
+
+        .info > .date {
+          font-size: 64px;
+          padding: 10px 0;
+        }
+
+        .info > button {
+          border: none;
+          background: orange;
+          color: #000;
+          padding: 10px 20px;
+          border-radius: 3px;
+          text-shadow: none;
+        }
+
+        .story {
+          min-height: 100px;
+          background-color: #ca9764;
+          margin-top: -90px;
+          padding: 50px;
+          box-sizing: border-box;
+          z-index: 3;
+          position: relative;
+        }
+
+        .story > .description > span {
+          line-height: 25px;
+          margin-top: 30px;
+          display: block;
+        }
+
+        .details {
+          padding: 50px;
+          color: #000;
+          min-height: 400px;
+          background: url("https://img5.goodfon.com/wallpaper/nbig/2/15/osen-listia-fon-colorful-background-autumn-leaves-osennie-26.jpg");
+          background-attachment: fixed;
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+        }
+
+        .details > span {
+          font-size: 26px;
+          border-bottom: 7px solid #f0b304;
+          border-radius: 7px;
+        }
+      `}</style>
     </div>
+  );
+};
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+export default Home;
