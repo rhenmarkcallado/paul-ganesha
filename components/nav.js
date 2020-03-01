503f30930a1dbdbbd50e649877e8912fdf9ddf9c
Rhen Mark Callado <rhenmarkc@gmail.com>
2020-03-01 21:59:14 +0800

    chore: partial changes

--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,18 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import Typist from "react-typist";
+import Icon from "@mdi/react";
+import { mdiMenu } from "@mdi/js";
+import { mdiClose } from "@mdi/js";
 
 const links = [
   {
     label: "Our Story",
     link: "story"
   },
-  {
-    label: "Proposal",
-    link: "proposal"
-  },
+  // {
+  //   label: "Proposal",
+  //   link: "proposal"
+  // },
   {
     label: "Details",
     link: "details"
@@ -24,22 +28,52 @@ const links = [
 ];
 
 const Nav = ({ top }) => {
+  const [isOpen, openMenu] = useState(false);
   const isScrolling = Boolean(top < -100);
+  const text = <span>Paul & Ganesha</span>;
+
+  const handleOnClickMenu = () => openMenu(!isOpen);
+
   return (
     <nav className={isScrolling ? "with-background" : ""}>
       <div className={` ${isScrolling ? "scrolling" : "logo"} `}>
         <a href="#">
-          Paul & <br /> Ganesha
+          {isScrolling ? (
+            text
+          ) : (
+            <Typist className="type">
+              {text}
+              <Typist.Backspace count={17} delay={1000} startDelay={300} />
+              <span>We are getting married!</span>
+              <Typist.Backspace count={26} delay={1000} />
+              <span> {text} </span>
+            </Typist>
+          )}
         </a>
       </div>
       <div className="navbar">
-        <ul>
-          {links.map(item => (
-            <li key={item.label}>
-              <a href={`#${item.link}`}>{item.label}</a>
-            </li>
-          ))}
-        </ul>
+        <div className="menu">
+          <button onClick={handleOnClickMenu} aria-label="Open">
+            <Icon
+              path={isOpen ? mdiClose : mdiMenu}
+              size={2}
+              horizontal
+              vertical
+              color="white"
+            />
+          </button>
+        </div>
+        <div className={`${isOpen ? "is-open" : ""}`}>
+          <ul>
+            {links.map(item => (
+              <li key={item.label}>
+                <a href={`#${item.link}`} rel="noopener">
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
       <style jsx>{`
@@ -49,7 +83,7 @@ const Nav = ({ top }) => {
           font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
             Helvetica, sans-serif;
           color: white;
-          background-color: #fff;
+          background-color: #000;
         }
 
         ul {
@@ -69,6 +103,7 @@ const Nav = ({ top }) => {
         .with-background {
           z-index: 10;
           background-color: rgba(0, 0, 0, 0.75);
+          transition: all linear 3s cubic-bezier(0.34, 0.95, 0.38, 0.3);
         }
 
         nav > .logo a {
@@ -77,15 +112,24 @@ const Nav = ({ top }) => {
           font-size: 32px;
         }
 
+        .Typist {
+          width: 205px !important;
+        }
+
         nav > .logo > a {
           position: absolute;
           padding-left: 62px;
           top: 160px;
-          font-size: 70px;
+          font-size: 48px;
           color: #ffffff;
           text-decoration-style: none;
           text-rendering: optimizeSpeed;
           text-shadow: -2px 2px 1px #ffa401;
+        }
+
+        .scrolling {
+          align-items: center;
+          display: flex;
         }
 
         nav > .scrolling > a {
@@ -94,21 +138,91 @@ const Nav = ({ top }) => {
           font-size: 18px;
           text-shadow: none;
           top: 0;
-          padding-left: 0;
+          padding: 10px 0;
           text-decoration: none;
           border: 1px solid #fff;
         }
 
-        nav > .navbar > ul {
+        nav > .navbar > div > ul {
           display: flex;
           flex-direction: row;
           justify-content: flex-end;
         }
 
-        nav > .navbar > ul > li a {
+        .menu {
+          display: none;
+        }
+
+        .menu > button {
+          background-color: transparent;
+          border-radius: 5px;
+          border: none;
+          position: relative;
+          z-index: 1;
+        }
+
+        nav > .navbar > div > ul > li a {
           margin-left: 30px;
           text-decoration: none;
           color: #fff;
+        }
+
+        @media only screen and (max-width: 780px) {
+          .navbar {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+          }
+
+          .menu {
+            display: block;
+          }
+
+          .navbar > div > ul {
+            display: none !important;
+          }
+
+          .navbar > .is-open {
+            display: flex !important;
+            background: rgba(208, 141, 13, 0.9803921568627451);
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            overflow: hidden;
+          }
+
+          .navbar > .is-open > ul {
+            display: flex !important;
+            flex-direction: column !important;
+            padding: 0;
+            margin: 0;
+            justify-content: flex-start !important;
+            width: 100% !important;
+            margin-top: 30%;
+            text-align: center;
+          }
+
+          .navbar > .is-open > ul > li,
+          a {
+            margin-left: 0 !important;
+            padding: 10px 0;
+            width: 100%;
+            display: block;
+            font-size: 24px;
+          }
+
+          .scrolling > a {
+            text-align: center;
+          }
+
+          nav > .logo > a {
+            left: 0;
+            padding: 0;
+            text-align: center;
+            right: 0;
+          }
         }
       `}</style>
     </nav>
--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,10 @@ const Home = () => {
     <div className="home">
       <Head>
         <title>Paul&Ganesha</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister|Pacifico&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Nav top={scrollTopPx} />
 
@@ -53,10 +57,46 @@ const Home = () => {
         </div>
       </div>
       <div className="details">
-        <span>Details</span>
+        <div className="events-details">
+          <div className="wedding-ceremony">
+            <span className="header">Wedding Ceremony</span>
+            <table>
+              <tr>
+                <td>DATE: </td>
+                <td>October 2, 2020</td>
+              </tr>
+              <tr>
+                <td>TIME: </td>
+                <td>01:00 PM</td>
+              </tr>
+              <tr>
+                <td>LOCATION: </td>
+                <td>
+                  Lokal ng Laoag City
+                  <br />
+                  <a
+                    href="https://www.google.com/maps/place/18%C2%B011'57.8%22N+120%C2%B035'09.8%22E/@18.1993446,120.5858307,129m/data=!3m1!1e3!4m5!3m4!1s0x0:0x0!8m2!3d18.199382!4d120.586056?hl=en-US"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    see direction
+                  </a>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div>
+            <span className="header">Reception</span>
+          </div>
+        </div>
       </div>
 
       <style jsx>{`
+        :global(body) {
+          font-family: "Love Ya Like A Sister" !important;
+          -webkit-overflow-scrolling: touch;
+        }
+
         .landing {
           background: url("https://images.unsplash.com/photo-1531747056595-07f6cbbe10ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80");
           background-size: cover;
@@ -67,17 +107,16 @@ const Home = () => {
           right: 0;
           top: -90px;
           z-index: 1;
-
           background-attachment: fixed;
-          background-position: center;
+          background-position: center center;
           background-repeat: no-repeat;
           background-size: cover;
         }
 
         .landing-container {
           display: flex;
-          justify-content: flex-end;
-          align-items: center;
+          justify-content: center;
+          align-items: flex-end;
           width: 100%;
           height: inherit;
           padding: 50px;
@@ -86,7 +125,7 @@ const Home = () => {
 
         .info {
           height: 300px;
-          width: 300px;
+          width: auto;
           color: #000;
           display: flex;
           justify-content: center;
@@ -98,8 +137,10 @@ const Home = () => {
         }
 
         .info > .date {
-          font-size: 64px;
+          font-size: 130px;
           padding: 10px 0;
+          color: #ffffff;
+          text-shadow: 4px 3px 3px #ff5722;
         }
 
         .info > button {
@@ -140,8 +181,86 @@ const Home = () => {
 
         .details > span {
           font-size: 26px;
-          border-bottom: 7px solid #f0b304;
-          border-radius: 7px;
+        }
+
+        .details > .events-details {
+          width: 100%;
+          min-height: 400px;
+          margin-top: 20px;
+          border-radius: 2px;
+          box-sizing: border-box;
+          display: grid;
+          grid-template-columns: 48% 48%;
+          justify-content: space-between;
+        }
+
+        .header {
+          display: block;
+          font-size: 32px;
+          color: #000;
+          margin-bottom: 20px;
+          background: #ffc002;
+          padding: 10px;
+          border: 1px solid #000;
+          color: #000000;
+          text-align: center;
+        }
+
+        .events-details > .wedding-ceremony > table {
+          width: 100%;
+          color: #fff;
+          border-collapse: collapse;
+          background-color: #ffc002;
+        }
+
+        .events-details > .wedding-ceremony > table > tr > td:nth-child(even) {
+          font-size: 32px;
+          text-shadow: 0px 3px 2px #582c0b;
+        }
+
+        .events-details
+          > .wedding-ceremony
+          > table
+          > tr
+          > td:nth-child(even)
+          > a {
+          font-size: 16px;
+          text-decoration: underline;
+          color: #000;
+          text-shadow: none;
+        }
+
+        .events-details > table > tr,
+        td {
+          font-size: 18px;
+          border: 1px solid #fff;
+          padding: 20px;
+          text-shadow: 1px 0px 1px #582c0b;
+        }
+
+        @media only screen and (max-width: 780px) {
+          .info > .date {
+            font-size: 68px;
+          }
+
+          .landing {
+            background-attachment: scroll;
+          }
+
+          .details {
+            background-attachment: scroll;
+            background-position: right;
+            padding: 10px;
+          }
+
+          .details > .events-details {
+            grid-template-columns: 100%;
+            padding: 0;
+          }
+
+          .wedding-ceremony {
+            margin-bottom: 50px;
+          }
         }
       `}</style>
     </div>
